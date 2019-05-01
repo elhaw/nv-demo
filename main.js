c98ed1f26bd00e59bee8dedd8faf44926f7e1336
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2019-05-01 16:31:48 +0200

    nitil commit

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
-$( window ).resize(function() {
-    if($(window).width() >= 768){
+$( document ).ready(function() {
+    // Handler for .ready() called.
+    if($(window).width() > 768){
+        if(this.width > 768){
             $('#profile').addClass('show')   
-    }
-  });
+
+        }
+}
+});
