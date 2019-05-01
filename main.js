52981f8efaed244d99450a7a9e21ee3d0e70b934
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2019-05-01 16:42:02 +0200

    initil

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 
-$( document ).ready(function() {
-    // Handler for .ready() called.
-    if($(window).width() > 768){
-        if(this.width > 768){
-            $('#profile').addClass('show')   
-
-        }
-}
-});
+// $( document ).ready(function() {
+//     // Handler for .ready() called.
+//     if($(window).width() > 768){
+//         if(this.width > 768){
+//             $('#profile').addClass('show')
+//         }
+// }
+// });
